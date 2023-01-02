6cf91783762e6695f795562fbd7b7b8945c31fc1
4562sky <4562sky@naver.com>
2023-01-02 14:15:52 +0900

    수업

---
+++ Chapter09 SearchAlgorithm/Setup/main.py
@@ -0,0 +1,39 @@
+from problem import *
+from optimizer import *
+
+def main():
+    p, pType = selectProblem()
+    alg = selectAlgorithm(pType)
+    alg.run(p)
+    p.describe()
+    alg.displaySetting()
+    
+
+def selectProblem():
+    print("Select the problem type:")
+    print(" 1. Numerical Optimization")
+    print(" 2. TSP")
+    pType = int(input("Enter the number: "))
+    if pType == 1:
+        p = Numeric()
+    elif pType == 2:
+        p = Tsp()
+    p.setVariables()
+    return p, pType
+
+def selectAlgorithm(pType):
+    print()
+    print("Select the search algorithm:")
+    print(" 1. Steepest-Ascent")
+    print(" 2. First-Choice")
+    print(" 3. Gradient Descent")
+    
+    while True:
+        aType = int(input("Enter the number: "))
+        if not invalid(pType, aType):
+            break
+    optimizers = { 1: 'SteepestAscent()', 
+                   2: 'FirstChoice()',
+                   3: 'GradientDescent()'}
+
+                   
